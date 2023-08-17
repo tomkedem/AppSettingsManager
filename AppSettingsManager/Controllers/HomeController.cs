c9bb0e6a9abb9013dfc362bbd42a828032478c4d
TomerK <tompha1@hotmail.com>
2023-08-17 21:54:12 +0300

    AppSetting And Model

--- AppSettingsManager/Controllers/HomeController.cs
+++ AppSettingsManager/Controllers/HomeController.cs
@@ -7,19 +7,34 @@ namespace AppSettingsManager.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
+        private TwilioSettings _twilioSettings;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
+           _twilioSettings = config.GetSection("Twilio").Get<TwilioSettings>();
         }
 
         public IActionResult Index()
         {
+            ViewBag.SendGridKey = _config.GetValue<string>("SendGridKey");
+            ViewBag.TwilioAuthToken = _config.GetSection("Twilio").GetValue<string>("AuthToken");
+            ViewBag.TwilioAccountSid = _config.GetValue<string>("Twilio:AccountSid");
+            ViewBag.TwilioPhoneNumber = _twilioSettings.PhoneNumber;
+            //ViewBag.BottomLevelSetting = _config.GetValue<string>("FirstLevelSetting:SecondLevelSetting:BottomLevelSetting");
+            //ViewBag.BottomLevelSetting = _config.GetSection("FirstLevelSetting").GetSection("SecondLevelSetting")
+            //                            .GetValue<string>("BottomLevelSetting");
+            ViewBag.BottomLevelSetting = _config.GetSection("FirstLevelSetting").GetSection("SecondLevelSetting")
+                                        .GetSection("BottomLevelSetting").Value;
+
             return View();
         }
 
         public IActionResult Privacy()
         {
+           
             return View();
         }
 
---
+++ AppSettingsManager/Models/TwilioSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace AppSettingsManager.Models
+{
+    public class TwilioSettings 
+    {
+        public string AuthToken { get; set; }
+        public string AccountSid { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+}
